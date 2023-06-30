10ce3815c52ff19a3e040acc556a86af35b74d1d
Kairat Alyshov <kairatalishev1308@gmaol.com>
2023-06-30 20:18:46 +0600

    added all packages with classes

---
+++ src/labWork/VoteMachineApp.java
@@ -0,0 +1,11 @@
+package labWork;
+
+import server.BasicServer;
+
+import java.io.IOException;
+
+public class VoteMachineApp extends BasicServer {
+    protected VoteMachineApp(String host, int port) throws IOException {
+        super(host, port);
+    }
+}
---
+++ src/server/ResponseCodes.java
@@ -0,0 +1,18 @@
+package server;
+
+public enum ResponseCodes {
+    OK(200),
+    NOT_FOUND(404),
+    REDIRECT(303);
+
+
+    private final int code;
+
+    ResponseCodes(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
