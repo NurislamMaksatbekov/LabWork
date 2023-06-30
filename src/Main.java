5d133cfce7d19ac46bb0accec1564a53cfd6f4bf
Nurislam <maksatbekkov@icloud.com>
2023-06-30 21:58:40 +0600

    added login methods and ftlh files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import labWork.VoteMachineApp;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            new VoteMachineApp("localhost", 9878).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/labWork/VoteMachineApp.java
+++ src/labWork/VoteMachineApp.java
@@ -1,11 +1,49 @@
 package labWork;
 
+import com.sun.net.httpserver.HttpExchange;
 import server.BasicServer;
+import server.Cookie;
+import util.FileService;
+import util.Utils;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class VoteMachineApp extends BasicServer {
-    protected VoteMachineApp(String host, int port) throws IOException {
+    public VoteMachineApp(String host, int port) throws IOException {
         super(host, port);
+        registerGet("/login", this::loginGet);
+        registerPost("/login", this::loginPost);
+    }
+
+    private void loginPost(HttpExchange exchange) {
+            String raw = getBody(exchange);
+            Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
+
+            String email = parsed.get("email");
+            String password = parsed.get("password");
+
+            if (user.stream().anyMatch(e -> e.getEmail().equals(email) && e.getPassword().equals(password))) {
+                Map<String, Object> data = new HashMap<>();
+                cookie = Cookie.make("email", email);
+
+                String cookieString = getCookies(exchange);
+                Map<String, String> cookies = Cookie.parse(cookieString);
+                cookie.setMaxAge(getMaxAge());
+                cookie.setHttpOnly(true);
+
+                setCookie(exchange, cookie);
+                data.put("cookies", cookies);
+
+                redirect303(exchange, "/profile?email=" + email);
+            } else {
+                redirect303(exchange, "/incorrectData");
+            }
+    }
+
+    private void loginGet(HttpExchange exchange) {
+        renderTemplate(exchange, "login.ftlh", null);
     }
 }
